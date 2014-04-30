940d7ddf1402d4d942379bb242f715a0db875783
Ishmael Smyrnow <ismyrnow@gmail.com>
2014-04-30 13:04:04 -0400

    Remove example plugins, add bootstrap
    - Removed alpha and beta jquery plugins.
    - Added bootstrap via shim config.
    - Adjusted tabs from 4 to 2 spaces.
    - Changed double quotes to single quotes.

--- www/js/app.js
+++ www/js/app.js
@@ -3,15 +3,15 @@
 // Configure loading modules from the lib directory,
 // except 'app' ones, 
 requirejs.config({
-    "baseUrl": "js/lib",
-    "paths": {
-      "app": "../app"
-    },
-    "shim": {
-        "jquery.alpha": ["jquery"],
-        "jquery.beta": ["jquery"]
-    }
+  'baseUrl': 'js/lib',
+  'paths': {
+    'app': '../app',
+    'bootstrap': 'bootstrap.min'
+  },
+  'shim': {
+    'bootstrap': ['jquery']
+  }
 });
 
 // Load the main app module to start the app
-requirejs(["app/main"]);
+requirejs(['app/main']);
--- www/js/app/main.js
+++ www/js/app/main.js
@@ -1,6 +1,5 @@
-define(["jquery", "jquery.alpha", "jquery.beta"], function($) {
-    //the jquery.alpha.js and jquery.beta.js plugins have been loaded.
-    $(function() {
-        $('body').alpha().beta();
-    });
+define(['jquery', 'bootstrap'], function($) {
+  $(function() {
+    $('body').append('jQuery and Bootstrap have been loaded.');
+  });
 });
